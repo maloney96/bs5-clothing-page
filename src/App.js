8c1ddc0d570f4f398aab70ad006fce176404e519
Liam Maloney <malonlia@oregonstate.edu>
2022-11-08 21:09:20 -0500

    Removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-// import logo from './logo.svg';
-// import Image from 'react-bootstrap/Image';
 import './App.css';
-// import {Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RecoFooter from './Recommend';
 import ClothOptions from './ClothOptions';
-
-// import Container from 'react-bootstrap/Container';
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
-
 
 function App() {
   return (

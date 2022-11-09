804e75556665572a3a3a015fb5e73582ba1a94e6
Liam Maloney <malonlia@oregonstate.edu>
2022-11-08 21:08:56 -0500

    Created clothing objects

---
+++ src/ItemsCollection.js
@@ -0,0 +1,22 @@
+
+
+let hats_json = {'Gucci Hat':'https://images.stockx.com/images/Gucci-Original-GG-Canvas-Baseball-Hat-with-Web-Beige-Brown.jpg?fit=fill&bg=FFFFFF&w=576&h=384&fm=avif&auto=compress&dpr=1&trim=color&trimcolor=ffffff&updated_at=1609366522&q=57'
+,'Carhartt Hat': 'https://m.media-amazon.com/images/I/71DREOUOMlL._AC_UX679_.jpg'
+,'Champion Hat': 'https://scene7.zumiez.com/is/image/zumiez/product_main_medium_2x/Champion-Classic-Twill-White-Strapback-Hat-_297377-front-US.jpg'
+}
+
+let shirts_json = {'Banana Republic Shirt': 'https://bananarepublic.gap.com/webcontent/0029/340/925/cn29340925.jpg'
+,'Patagonia T-Shirt': 'https://dks.scene7.com/is/image/GolfGalaxy/21PTGWWLSP6LGRSPNAPT_Rosehip?qlt=70&wid=600&fmt=pjpeg'
+,'Alphalete Hoodie': 'https://cdn.shopify.com/s/files/1/0667/0133/products/MenHoodie_009_1000x.jpg?v=1624634903'
+}
+
+let pants_json = {'Levi Jeans': 'https://lsco.scene7.com/is/image/lsco/A22460001-alt7-pdp?$qv_desktop_full$'
+,'Alphalete Pants': 'https://cdn.shopify.com/s/files/1/0605/4880/8939/products/jpg_e6bf4e22-1349-4340-97bd-e50663e12c2e_800x.jpg?v=1661413609'
+,'Lululemon Shorts': 'https://images.lululemon.com/is/image/lululemon/LM7AHLS_051415_1?wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72'}
+
+let shoes_json = {'Cream Yeezys': 'https://cdn.shopify.com/s/files/1/0255/9429/8467/products/adidas-originals-yeezy-boost-350-v2-cream-white-CP9366_6.jpg?v=1658250091&width=1024'
+,'Timberland Boots': 'https://m.media-amazon.com/images/S/aplus-media/sota/c53cc11b-f621-4cc1-a14f-ba989bb9fb13.__CR154,0,970,600_PT0_SX970_V1___.png'
+,'Adidas Boost Slides': 'https://assets.adidas.com/images/w_600,f_auto,q_auto/223ea2b54c5842cfb94fac0800eca4ea_9366/Adilette_Boost_Slides_White_FY8155_06_standard.jpg'
+}
+
+export {hats_json, shirts_json, pants_json, shoes_json}

e960b1d7f35f858056efbef6e4b0d18c50bc617e
Liam Maloney <malonlia@oregonstate.edu>
2023-02-25 18:13:22 -0500

    Cleanup comments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import RecoFooter from './Recommend';
-// import ClothOptions from './ClothOptions';
-import WeatherForm from './Weather';
-// import Header from './Header';
-// import Reccomend from './Recommend';
 
-// const weather = handle_weather.weather
-// const handleSubmit = handle_weather.handleSubmit
+import WeatherForm from './Weather';
+
 
 function App() {
   return (
@@ -17,11 +12,8 @@ function App() {
         Clothing Recommender
       </p>
       </header>
-      {/* <Header weather = {weather} handleSubmit = {handleSubmit}></Header> */}
       <WeatherForm></WeatherForm>
       <main>
-      {/* <ClothOptions></ClothOptions> */}
-      {/* <Reccomend></Reccomend> */}
       </main>
     </div>
   );
